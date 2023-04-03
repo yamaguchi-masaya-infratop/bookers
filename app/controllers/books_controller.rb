fd552934220f3930142579733e96d7a362757348
EC2 Default User <ec2-user@ip-172-31-4-234.ap-northeast-1.compute.internal>
2023-04-03 09:09:15 +0000

    [Add]Basic Functions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
   def create 
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "投稿が成功しました"
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      flash[:notice] = "投稿が失敗しました"
+      render :index
     end
   end
   
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -22,6 +22,29 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update 
+    @book = Book.find(params[:id])
+    if @book.update(book_params) 
+      flash[:notice] = "編集が成功しました"
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.find(params[:id])
+      flash[:notice] = "編集が失敗しました"
+      render :edit
+    end
+  end
+  
+  def destroy 
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "削除が成功しました"
+      redirect_to '/books'
+    else 
+      flash[:notice] = "削除が失敗しました"
+      render :index
+    end
   end
   
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   #get 'books/edit'
   root to: 'homes#top'
   resources :books
+  #patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
